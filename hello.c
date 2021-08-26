29f83b85093eb83cb41129ed2652dd518267c466
Grady Henexson <ghenexson2@huskers.unl.edu>
2021-08-26 23:55:14 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+* Authors: Grady Henexson & Kolton O'Neal
+* Date: 2021/08/24
+*
+* Hack 1.0 Program
+*
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Hello World, my name is Grady Henexson!")
+    printf("My major is Computer Science!\n");
+    return 0;
+}
